dc8a6ec2bca24aec2f27521870c7e555a0eef1ae
Yusuf Yusufov <yusitoo1234@gmail.com>
2022-01-10 18:29:56 +0200

    Setter injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,10 +8,25 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//define default constructor
+	public TennisCoach() {
+		System.out.println(">>tennisCoach:  inside default constructor");
+	}
+	
+	//define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">>tennisCoach:  inside the setter " );
+
+		fortuneService = theFortuneService;
+	}
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
